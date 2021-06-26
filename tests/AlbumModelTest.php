a007842b3e063661aecc914e5d69fd554dac10e6
Nicat <nicatnagizade53@gmail.com>
2021-06-26 18:22:38 +0200

    created relation between album and photos

--- tests/AlbumModelTest.php
+++ tests/AlbumModelTest.php
@@ -12,9 +12,15 @@ class AlbumModelTest extends TestCase
         parent::setUp();
         $this->model = new Album;
     }
-    public function testGetAllAlbums()
+    public function test_get_all_albums()
     {
         $albums = $this->model->get();
         $this->assertInstanceOf(Collection::class, $albums);
+    }
+    public function test_album_with_photos()
+    {
+        $albums = $this->model->withPhotos();
+        $this->assertInstanceOf(Collection::class, $albums);
+        dd($albums);
     }
 }
