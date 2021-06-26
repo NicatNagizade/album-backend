bb48abc2aa2686d2d416aab067d1aec866dbb1d1
Nicat <nicatnagizade53@gmail.com>
2021-06-26 21:21:43 +0200

    created audit log

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
 // $app->withEloquent();
 
@@ -79,6 +79,7 @@ $app->configure('app');
 $app->routeMiddleware([
     // 'auth' => App\Http\Middleware\Authenticate::class,
     'cors' => App\Http\Middleware\CorsMiddleware::class,
+    'audit.log' => App\Http\Middleware\AuditLogMiddleware::class,
 ]);
 
 /*
