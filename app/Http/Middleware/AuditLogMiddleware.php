853a5d2d61d265728b9f3b2dbdbc0098f79b4066
Nicat <nicatnagizade53@gmail.com>
2021-06-27 11:08:34 +0200

    updated audit log

--- app/Http/Middleware/AuditLogMiddleware.php
+++ app/Http/Middleware/AuditLogMiddleware.php
@@ -5,20 +5,27 @@ namespace App\Http\Middleware;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AuditLogMiddleware
 {
     public function handle($request, Closure $next)
     {
-        DB::table('audit_logs')->insert([
-            'ip' => $request->ip(),
-            'endpoint' => $request->url(),
-            'method' => $request->method(),
-            'payload' => json_encode($request->all()),
-            'header' => json_encode($request->header()),
-            'created_at' => Carbon::now()
-        ]);
         $response = $next($request);
+        try {
+            DB::table('audit_logs')->insert([
+                'ip' => $request->ip(),
+                'endpoint' => $request->path(),
+                'method' => $request->method(),
+                'status' => $response->getStatusCode(),
+                'payload' => json_encode($request->all()),
+                'header' => json_encode($request->header()),
+                'response_time' => (microtime(true) - LUMEN_START),
+                'created_at' => Carbon::now()
+            ]);
+        } catch (\Exception $ex) {
+            Log::error('[audit_logs]: '.$ex->getMessage());
+        }
         return $response;
     }
 }
